214999ece770c244861f858a76152069a0c6a744
Christ Joe Santos <christjoesantos2000@gmail.com>
2023-11-02 20:00:27 +0800

    add ai function 3rd party

---
+++ haircut.py
@@ -0,0 +1,110 @@
+import requests
+import time
+import urllib
+import base64
+
+BASE_URL = "https://www.ailabapi.com"
+HAIRCUT_API_ENDPOINT = "/api/portrait/effects/hairstyle-editor-pro"
+GET_ASYNC_TASK_QUERY_ENDPOINT = "/api/common/query-async-task-result"
+API_KEY = "OXqnSbcJaGN0CazUDOrcmNxMZRiyPrpjTb4R3Ii6JoWdBZA7B59LYV5netsQl1lz"
+HAIRCUTS = {
+    1: "BuzzCut",
+    2: "LowFade",
+    3: "TexturedFringe",
+    4: "Pompadour",
+    5: "UnderCut",
+    6: "SlickBack",
+}
+
+
+def generate_haircut_ai(*, image, haircut: int):
+    headers = {"ailabapi-api-key": API_KEY}
+    hairstyle = HAIRCUTS.get(int(haircut))
+    data = {
+        "task_type": "async",
+        "hair_style": hairstyle,
+        "reference": "1",
+    }
+    files = {"image": image}
+
+    try:
+        result = requests.post(
+            BASE_URL + HAIRCUT_API_ENDPOINT,
+            headers=headers,
+            files=files,
+            data=data,
+        )
+        if result.status_code != 200:
+            return "Failed to generate haircut: " + result.text
+
+        if result.json().get("error_code") != 0:
+            return "Failed to generate haircut: " + result.json().get("error_msg")
+
+        task_id = result.json().get("task_id")
+        task_result = get_haircut_image_async_task_result(task_id=task_id)
+
+        if isinstance(task_result, str):
+            return "Failed to generate haircut: " + task_result
+
+        generated_image = task_result.get("data").get("images")[0]
+        if not generated_image:
+            return "Failed to generate haircut: " + task_result.get("error_msg")
+
+        image_path, _ = urllib.request.urlretrieve(generated_image, "haircut-image.jpg")
+
+        return image_to_base64(image_path=image_path)
+
+    except Exception as e:
+        print(e)
+        return "Failed to generate haircut: " + str(e)
+
+
+def get_haircut_image_async_task_result(*, task_id: str):
+    headers = {"ailabapi-api-key": API_KEY}
+    max_retries = 20  # Define the maximum number of retries
+    retry_interval = 2  # Retry every 2 seconds
+
+    for retry in range(max_retries):
+        try:
+            result = requests.get(
+                BASE_URL + GET_ASYNC_TASK_QUERY_ENDPOINT,
+                headers=headers,
+                params={"task_id": task_id},
+            )
+
+            if result.status_code != 200:
+                return "Failed to haircut result image: " + result.text
+
+            task_status = result.json().get("task_status")
+
+            if task_status == 2:
+                return result.json()
+            else:
+                if retry < max_retries - 1:
+                    # Task is still running, retry after the specified interval
+                    print(
+                        "Task is still running, retrying in "
+                        + str(retry_interval)
+                        + " seconds"
+                    )
+                    time.sleep(retry_interval)
+                    continue
+
+            # If task_status is not 1 or 2, or max retries are exhausted, return an error message
+            return "Failed to get generate haircut: Task status is not 1 or 2"
+
+        except Exception as e:
+            print(e)
+            if retry < max_retries - 1:
+                # Retry if an exception occurred and we haven't exhausted retries
+                time.sleep(retry_interval)
+                continue
+            return "Failed to get generate haircut: " + str(e)
+
+    return "Failed to get generate haircut: Maximum retries reached"
+
+
+def image_to_base64(*, image_path: str):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+        return encoded_string
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import base64
 import os
 import requests
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from haircut import (
+    generate_haircut_ai,
+)
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,33 +17,11 @@ MAX_CONTENT_LENGTH = 3 * 1024 * 1024
 
 base_url = "https://semaphore.co/api/v4/messages"
 api_key = "d976ae4a30b895246d9fc1bc1dabacdb"
+allowed_extensions = {"jpg", "jpeg", "png"}
 
 
-@app.route("/send_sms", methods=["POST"])
-def send_sms():
-    # address = get_client_location()
-    # return address
-    sender_name = "SEMAPHORE"
-    message = "Test Message"  # Replace Message Template
-    number = "09511808537"  # "09511808537,09915236234"  # Get from firebase
-    params = (
-        ("apikey", api_key),
-        ("sendername", sender_name),
-        ("message", message),
-        ("number", number),
-    )
-
-    response = requests.post(
-        base_url,
-        params=params,
-    )
-    return response.text
-
-
-# Define the route for uploading an image
-@app.route("/upload", methods=["POST"])
-def upload_image():
-    # Check if the 'image' field exists in the request
+@app.route("/generate-haircut", methods=["POST"])
+def ai_generate_haircut():
     if "image" not in request.files:
         return jsonify({"error": "No image part"})
 
@@ -47,12 +29,9 @@ def upload_image():
     image = request.files["image"]
     haircut = request.form.get("haircut")
 
-    # Check if the file is empty
     if image.filename == "":
         return jsonify({"error": "No selected file"})
 
-    # Check if the file is an allowed format (e.g., you can add more formats)
-    allowed_extensions = {"jpg", "jpeg", "png"}
     if (
         "." not in image.filename
         or image.filename.rsplit(".", 1)[1].lower() not in allowed_extensions
@@ -60,7 +39,46 @@ def upload_image():
         return jsonify({"error": "Invalid file format"})
 
     # Check if the file size is within the allowed limit
-    # if len(image.read()) > app.config["MAX_CONTENT_LENGTH"]:
+    if len(image.read()) > MAX_CONTENT_LENGTH:
+        return jsonify({"error": "File size exceeds the 3MB limit"})
+
+    # Reset the file cursor to read the image content again
+    image.seek(0)
+
+    # You can process the image here (optional)
+    image_data = generate_haircut_ai(image=image, haircut=haircut)
+
+    # # Read the image content and encode it as base64
+    # image_data = image_to_base64(image=img_generated_haircut)
+
+    # Create a JSON response with the base64-encoded image
+    response_data = {
+        "image_data": image_data,
+        "haircut": haircut,
+    }
+
+    return jsonify(response_data)
+
+
+@app.route("/upload", methods=["POST"])
+def upload_image():
+    if "image" not in request.files:
+        return jsonify({"error": "No image part"})
+
+    # Request Payload
+    image = request.files["image"]
+    haircut = request.form.get("haircut")
+
+    if image.filename == "":
+        return jsonify({"error": "No selected file"})
+
+    if (
+        "." not in image.filename
+        or image.filename.rsplit(".", 1)[1].lower() not in allowed_extensions
+    ):
+        return jsonify({"error": "Invalid file format"})
+
+    # Check if the file size is within the allowed limit
     if len(image.read()) > MAX_CONTENT_LENGTH:
         return jsonify({"error": "File size exceeds the 3MB limit"})
 
